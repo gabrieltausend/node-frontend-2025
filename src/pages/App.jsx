e5bc755a44db5e1c92f50fe3f79d9f3b42962b28
gabrieltausend <gabriel.tausen@hotmail.com>
2025-12-17 22:40:36 -0300

    commit final 6

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,23 +2,33 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useAuth } from "../auth/useAuth";
 
+// IMPORT DAS IMAGENS
+import logo from "../assets/logo.png";
+import crn1 from "../assets/crn1.png";
+import crn2 from "../assets/crn2.png";
+import crn3 from "../assets/crn3.png";
+
 const App = () => {
     const { user } = useAuth();
+
     return (
         <div>
             <Navbar />
+
             <div className="flex justify-center items-center mt-8">
                 <img
-                    src="/logo.png"
+                    src={logo}
                     alt="Logo"
                     className="w-30 h-auto"
                 />
             </div>
+
             <div className="flex flex-col items-center mt-6">
                 <Link to="/chamados/create" className="btn btn-secondary m-4">
                     Criar um Pedido
                 </Link>
             </div>
+
             <div
                 id="carouselExampleControls"
                 className="carousel slide"
@@ -28,25 +38,28 @@ const App = () => {
                     <div className="carousel-item active">
                         <img
                             className="d-block w-50 mx-auto"
-                            src="/crn1.png"
+                            src={crn1}
                             alt="Fachada 1 CRN"
                         />
                     </div>
+
                     <div className="carousel-item">
                         <img
                             className="d-block w-50 mx-auto"
-                            src="/crn2.png"
+                            src={crn2}
                             alt="Fachada 2 CRN"
                         />
                     </div>
+
                     <div className="carousel-item">
                         <img
                             className="d-block w-50 mx-auto"
-                            src="/crn3.png"
+                            src={crn3}
                             alt="Fachada 3 CRN"
                         />
                     </div>
                 </div>
+
                 <button
                     className="carousel-control-prev"
                     type="button"
@@ -65,9 +78,14 @@ const App = () => {
                     <span className="carousel-control-next-icon"></span>
                 </button>
             </div>
+
             <h2 className="mx-2">Contatos</h2>
-            <h3 className="mx-3">Email: contato@acprojeto.com.br, Instagram: acprojetoseconstrucoes, Whatsapp: (49)99907-6820</h3>
+            <h3 className="mx-3">
+                Email: contato@acprojeto.com.br, Instagram: acprojetoseconstrucoes,
+                Whatsapp: (49)99907-6820
+            </h3>
         </div>
     );
 };
+
 export default App;
